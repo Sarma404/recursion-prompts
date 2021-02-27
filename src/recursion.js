8477c27d19f87bb2ceca7c3058b2be0afb5ba635
Aleksandar Cakic <acakic92@gmail.com>
2021-02-27 15:38:36 -0600

    Add isEven recursive function

--- src/recursion.js
+++ src/recursion.js
@@ -43,10 +43,26 @@ var sum = function(array) {
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  console.log(array)
+  if (array.length === 0) {
+    return 0;
+
+    if (Array.isArray(array[0])) {
+      array.slice(0);
+    }
+    return array[0] + arraySum(array.slice(1));
+  }
 };
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+  if (n === 0) {
+    return true;
+  }
+  if (n === 1) {
+    return false;
+  }
+  return isEven(Math.abs(n-2));
 };
 
 // 5. Sum all integers below a given integer.
